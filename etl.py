cb2d560a9016f65074654312cf9de7b7579bffdd
Daniel Miller <danielmiller5791@gmail.com>
2019-08-26 16:59:03 +0100

    song frames created, diagramming out then implementing song frames

--- etl.py
+++ etl.py
@@ -3,14 +3,10 @@ from multiprocessing import Pool
 import multiprocessing
 import subprocess
 import re
-from joblib import Parallel, delayed
-from lib.file_finder import FileFinder
 from lib.rdd_creator import RDDCreator
+from lib.file_finder import FileFinder
+from pyspark.sql import SparkSession
 import os
-import pdb
-import csv
-from datetime import datetime
-import pandas as pd
 
 def extract_files_from_s3(directories):
     with Pool(processes=multiprocessing.cpu_count()) as pool:
@@ -21,28 +17,42 @@ def fetch_files_from_s3(suffix):
   for the log and song directories that are in s3, downloads them to the tmp directory, 
   suffix is the log and song directory directory in s3
   '''
-  local_path = os.getcwd() + '/tmp' + f'/{suffix}'
-  subprocess.run(f'aws s3 sync s3://udacity-dend/{suffix} {local_path}', shell=True, check=True)
+  local_path = os.getcwd() + '/tmp/{}'.format(suffix)
+  subprocess.run('aws s3 sync s3://udacity-dend/{} {}'.format(suffix, local_path), shell=True, check=True)
 
 
 def return_file_names(directory):
-  file_finder = FileFinder(os.getcwd() + f'/tmp/{directory}/', '*.json')
+  file_finder = FileFinder(os.getcwd() + '/tmp/{}/'.format(directory), '*.json')
   return list(file_finder.return_file_names())
 
 def main():
 
+  spark = SparkSession\
+    .builder\
+    .appName("dev_app")\
+    .getOrCreate()\
+
+  spark.sparkContext.setLogLevel("ERROR")
+  
   directories = ['log_data', 'song_data']
   dataframes = {}
   # extract_files_from_s3(directories)
 
-  frames = map(lambda dir: RDDCreator(dir, return_file_names(dir)), directories)
+  frames = map(lambda dir: RDDCreator(dir, return_file_names(dir), spark), directories)
 
   frames = map(
     lambda frame: frame.create_rdd_from_path(), frames
   )
 
-  for f in frames:
-    print(f)
+  song_frame = list(frames)[1]
+
+  song_frame.write.csv(os.getcwd() + '/sample_song_frame.csv')
+  
+  print('exiting application')
+  spark.stop()
+
+
+
 
 
 
---
+++ zip_copy.sh
@@ -0,0 +1,3 @@
+zip -r project.zip *.py lib/*.py
+scp -i sparkify-lake-2.pem project.zip hadoop@ec2-107-21-88-138.compute-1.amazonaws.com:~
+rm project.zip
